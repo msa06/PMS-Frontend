d923e9f462c7d5da6203bcdf851d41b4c2730199
Mohammad Suhaib Ahmed <suhaib.md06@gmail.com>
2019-02-22 16:26:49 +0530

    Added Sidenav and Navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule,Router, Routes} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WorkorderComponent } from './components/workorder/workorder.component';
+import { AssetsComponent } from './components/assets/assets.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+const appRoutes:Routes = [
+  {path:'',component:WorkorderComponent},
+  {path:'assets',component:AssetsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WorkorderComponent,
+    AssetsComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
